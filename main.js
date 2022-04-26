d1e36d6f88ec587dd396aa93b32a1253dffecc32
harsha383 <82648520+harsha383@users.noreply.github.com>
2022-04-26 17:37:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+status_object = "";
+input = "";
+
+function preload() {
+
+}
+function setup() {
+    canvas = createCanvas(480, 380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+function draw() {
+    image(video, 0, 0, 480, 380);
+}
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    input = document.getElementById("input").value;
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status_object = true;
+}
